b79be813177ecd7b20c6ed6625cceb98e313a879
joshuaSmith2021 <joshuasmith6556@gmail.com>
2021-02-07 21:56:00 -0800

    pogchamps schedule

--- chess.py
+++ chess.py
@@ -1,6 +1,8 @@
+import datetime
 import json
 import re
 
+from bs4 import BeautifulSoup
 import requests
 
 def get_user_stats(username):
@@ -29,6 +31,34 @@ def record_string(data):
     return '/'.join([str(data['record'][x]) for x in ['win', 'loss', 'draw']])
 
 
+def pogchamp_schedule_to_csv():
+    with open('data/pogchamps/pogchamps3table.html') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+
+    rows = soup.findAll('tr', {'class': ['standings_odd_row', 'standings_even_row']})
+
+    csv = [
+        ('Subject', 'Start Date', 'Start Time', 'End Time', 'Description')
+    ]
+
+    for row in rows:
+        cells = [x.text for x in row.findChildren()]
+        if '-' in cells:
+            # Skip the pre-match hype event for now
+            continue
+
+        day, dow, start, end, group, mnum, p1, p2 = cells
+        csv.append((
+            f'{p1} vs {p2}',
+            f'02/{day.split()[1]}/2021',
+            f'{start} {"AM" if start[:2] == "11" else "PM"}',
+            f'{end} {"AM" if end[:2] == "11" else "PM"}',
+            f'{group}: {mnum}'
+        ))
+
+    return '\n'.join(map(lambda x: ','.join(x), csv))
+
+
 def build_embed(embed, user_data):
     '''Create a Discord embed with user data displayed.
     embed should be a Discord embed object.
@@ -54,5 +84,8 @@ def build_embed(embed, user_data):
 if __name__ == '__main__':
     user = get_user_stats('magnuscarlsen')
     # print(get_elos(user))
-    build_embed(None, user)
+    # build_embed(None, user)
+    with open('data/pogchamps/poghamps3schedule.csv', 'w') as f:
+        schedule = pogchamp_schedule_to_csv()
+        f.write(schedule)
 
